7a0c685f9bcc0438f98b5f025b6b1ef30350665a
menooing.dev <menooing.dev@gmail.com>
2018-01-02 22:02:42 -0600

    init

---
+++ index.php
@@ -0,0 +1,213 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Korean Software Developer Community @DFW</title>
+
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">KSDC@DFW</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+
+
+
+
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Member</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Bryant(Sr. Developer,Citi)</a>
+              <a class="dropdown-item" href="#">Ricky Kim(Sr. Developer,Lennox)</a>
+              <a class="dropdown-item" href="#">송현술(Sr. Developer,StateFarm)</a>
+              <a class="dropdown-item" href="#">Namho Jung(Sr. Developer,TCS Bank Of America)</a>
+              <a class="dropdown-item" href="#">최호원(Sr. Developer,ESI Tech)</a>
+              <a class="dropdown-item" href="#">채민호(Sr. Developer,Fluo labs)</a>
+              <a class="dropdown-item" href="#">성영하 (Sr Developer,Nationwide Mortgage)</a>
+              <a class="dropdown-item" href="#">김영태(Mid. Developer,Lockheed Martin)</a>
+              <a class="dropdown-item" href="#">김기범(Mid. Developer,Elevator Credit)</a>
+              <a class="dropdown-item" href="#">John Lee(Mid. Developer,Citi)</a>
+              <!--a class="dropdown-item" href="#">Cs Sin(Sr.SW Engineer)</a-->
+              <!--a class="dropdown-item" href="#">Billy Hong(Sr.SW Engineer)</a-->
+              <!--a class="dropdown-item" href="#">Greg Bun(Sr.SW Engineer)</a-->
+              <a class="dropdown-item" href="#">신승진(Jr. Developer)</a>
+              <a class="dropdown-item" href="#">Jay ParK(Jr. Developer)</a>
+              <a class="dropdown-item" href="#">Justin Lee(Student)</a>
+
+
+
+
+
+            </div>
+          </li>
+
+          <!--li class="nav-item ">
+            <a class="nav-link " href="int.html" target="_new" >Interview Tip</a>
+          </li-->
+        </ul>
+
+      </div>
+    </nav>
+
+    <main role="main">
+<br>
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-5">KSDC@DFW  </h1>
+          <p>DFW 지역 한인 소프트웨어 개발자들이 함께 모여 서로의 고민과 정보를 나누는 모임 </p>
+
+          <p>2018 전체모임 일정입니다.
+            참고 하시고 스케줄 조정 부탁드리겠습니다.
+              <a href="rsvp/"><button type="button" class="btn btn-primary" >+ Click RSVP</button></a>
+
+          </p>
+        </div>
+      </div>
+
+
+
+
+
+
+
+      <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-6">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>개발자 포럼</h2>
+            <p>DFW지역 기업에 근무하는 소프트웨어 개발자 모임</p>
+            <ul>
+            <li>대상: SW 개발자 && 개발자 준비중인 분</li>
+            <li>개발자 Power Up(Network/Tech)</li>
+           </ul>
+
+
+<div class="table-responsive">
+
+
+<table class="table table-striped">
+<thead>
+<tr>
+<th>#</th>
+<th>일정</th>
+<th>장소</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td>1</td>
+<td>7:00pm, 1/18/2018</td>
+<td>수라 2층 연회장
+<br>회비:Sr $30, Mid $20, Jr/Student: $10
+</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>7:00pm, 4/19/2018</td>
+<td>TBD</td>
+</tr>
+
+
+<tr>
+<td>3</td>
+<td>7:00pm, 7/26/2018</td>
+<td>TBD</td>
+</tr>
+
+
+<tr>
+<td>4</td>
+<td>7:00pm, 10/25/2018</td>
+<td>TBD</td>
+</tr>
+
+</tbody>
+</table>
+</div>
+
+
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-6">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>비니지스 포럼</h2>
+            <p>DFW 한인 개발자와 비지니스 전문가들의 연합모임
+            </p>
+            <ul>
+            <li>대상: 개발자 && 비지니스 전문가</li>
+            <li>DFW 비지니스 연결및 스타트업 육성</li>
+           </ul>
+            <p><a class="btn btn-secondary" href="#" role="button">TBD</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+
+
+
+
+        <hr class="featurette-divider">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+
+
+    </main>
+
+    <footer class="container">
+      <p>&copy; Korean Software Developer Community @DFW 2017</p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
